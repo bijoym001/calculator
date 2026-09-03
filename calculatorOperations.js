55551f42c356d82373e56491dd0d9023c6c7dd96
Bijoy Mukherjee <bmukherjee014@gmail.com>
2026-09-03 10:21:25 +0530

    First Commit

---
+++ calculatorOperations.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+
+
+module.exports = {
+    add,
+    subtract,
+    multiplication,
+    division
+}
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const {
+    add,
+    subtract,
+    multiplication,
+    division,
+} = require("../calculator/calculatorOperations");
+
+
+const operations = process.argv[2];
+const num1 = Number(process.argv[3])
+const num2 = Number(process.argv[4])
+
+
+switch(operations){
+    case "add":
+        console.log(add(num1,num2))
+        break
+    case "subtract":
+        console.log(subtract(num1,num2))
+        break
+    case "multiplication":
+        console.log(multiplication(num1,num2))
+        break
+    case "division":
+        console.log(division(num1, num2))
+        break
+    default:
+        console.log("Error!")
+
+}
+
